9d40a2084ed9093d63a1de023eddbccbdba92940
Nont <apple@NontTechops.local>
2020-05-01 05:43:17 +0700

    Add codebuild / codedeploy specs

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,6 +5,21 @@ REGION=ap-southeast-1
 CLI_PROFILE=awsboostrap
 
 EC2_INSTANCE_TYPE=t2.micro
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsboostrap \
+--query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploy static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+--region $REGION \
+--profile $CLI_PROFILE \
+--stack-name $STACK_NAME-setup \
+--template-file setup.yml \
+--no-fail-on-empty-changeset \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameter-overrides \
+CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
